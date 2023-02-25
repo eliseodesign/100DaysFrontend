bce300bbe6b2fc4aa97155209ab39d68ef7a9480
Eliseo Arévalo <2480931@clases.edu.sv>
2023-02-25 08:38:34 -0600

    [:sparkles: config] RRD, Data.js, Views, VarsSCss

--- proyectos/5_notifications/src/App.tsx
+++ proyectos/5_notifications/src/App.tsx
@@ -1,8 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import List from "./views/list"
+import Details from "./views/details"
+
 function App() {
   
   return (
     <div className="App">
      <h1>Noitifications</h1>
+
+   
+      <BrowserRouter>
+        <Routes>  
+          <Route path="/" element={<List/>}/>
+          <Route path="/details/:id" element={<Details />}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
---
+++ proyectos/5_notifications/src/utils/data.js
@@ -0,0 +1,37 @@
+const notifications = [
+  {
+    user: 'Mark Webber',
+    message: 'reacted to your recent post My first tournament today!',
+    time: '1m ago'
+  },
+  {
+    user: 'Angela Gray',
+    message: 'followed you',
+    time: '5m ago'
+  },
+  {
+    user: 'Jacob Thompson',
+    message: 'has joined your group Chess Club',
+    time: '1 day ago'
+  },
+  {
+    user: 'Rizky Hasanuddin',
+    message: 'sent you a private message\nHello, thanks for setting up the Chess Club. I\'ve been a member for a few weeks now and\nI\'m already having lots of fun and improving my game.',
+    time: '5 days ago'
+  },
+  {
+    user: 'Kimberly Smith',
+    message: 'commented on your picture',
+    time: '1 week ago'
+  },
+  {
+    user: 'Nathan Peterson',
+    message: 'reacted to your recent post 5 end-game strategies to increase your win rate',
+    time: '2 weeks ago'
+  },
+  {
+    user: 'Anna Kim',
+    message: 'left the group Chess Club',
+    time: '2 weeks ago'
+  }
+];
